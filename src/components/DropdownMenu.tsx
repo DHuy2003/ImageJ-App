b32374e9bd645302671887b0c6386d12e738e1b2
DHuy2003 <duchuy07062003@gmail.com>
2025-10-11 17:26:10 +0700

    dropdown-menu

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,22 @@
+type DropdownMenuProps = {
+    label: string;           
+    items?: string[];         
+}
+
+const DropdownMenu = ({ label, items = [] }: DropdownMenuProps) => {
+    return (
+        <li className="dropdown">
+          <a href="#">{label}</a>
+          {items && items.length > 0 && (
+            <ul className="dropdown-menu">
+              {items.map((item, index) => (
+                <li key={index}>
+                  <a href="#">{item}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+    );
+}
+export default DropdownMenu;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { BarChart, BarChart3, Microscope, Search } from "lucide-react";
+import {BarChart3, Microscope, Search } from "lucide-react";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
@@ -14,7 +14,7 @@ const NavBar = () => {
             <ul id="navbar-menu">
                 <li>
                     <a href="#">File</a>
-                    {/* <ul id="dropdown-menu">
+                    <ul className="dropdown-menu">
                         <li><a href="">New</a></li>
                         <li><a href="">Open</a></li>
                         <li><a href="">Open Next</a></li>
@@ -26,13 +26,58 @@ const NavBar = () => {
                         <li><a href="">Revert</a></li>
                         <li><a href="">Page Setup</a></li>
                         <li><a href="">Quit</a></li>
-                    </ul> */}
+                    </ul>
                 </li>
 
-                <li><a href="#">Edit</a></li>
-                <li><a href="#">Image</a></li>
-                <li><a href="#">Analyze</a></li>
-                <li><a href="#">Process</a></li>
+                <li>
+                    <a href="#">Edit</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="">Undo</a></li>
+                        <li><a href="">Cut</a></li>
+                        <li><a href="">Clear</a></li>
+                        <li><a href="">Clear Outside</a></li>
+                        <li><a href="">Fill</a></li>
+                        <li><a href="">Draw</a></li>
+                        <li><a href="">Invert</a></li>
+                        <li><a href="">Selection</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#">Image</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="">Color</a></li>
+                        <li><a href="">Type</a></li>
+                        <li><a href="">Zoom</a></li>
+                        <li><a href="">Show Info...</a></li>
+                        <li><a href="">Duplicate...</a></li>
+                        <li><a href="">Rename</a></li>
+                        <li><a href="">Tranform</a></li>
+                        <li><a href="">Adjust</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Analyze</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="">Measure</a></li>
+                        <li><a href="">Lable</a></li>
+                        <li><a href="">Clear Results</a></li>
+                        <li><a href="">Set Measurements...</a></li>
+                        <li><a href="">Set Scale...</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#">Process</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="">Smooth</a></li>
+                        <li><a href="">Sharpen</a></li>
+                        <li><a href="">Enhance Contrast...</a></li>
+                        <li><a href="">Subtract Background...</a></li>
+                        <li><a href="">Filters</a></li>
+                        <li><a href="">Binary</a></li>
+                    </ul>
+                </li>
             </ul>
 
             <div className="divider"></div>
