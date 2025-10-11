1ee0d6f7bea1d56a7a06472267e031d14c762c1f
DHuy2003 <duchuy07062003@gmail.com>
2025-10-11 11:03:08 +0700

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,54 @@
-import { Microscope } from "lucide-react";
+import { BarChart, BarChart3, Microscope, Search } from "lucide-react";
+import "../styles/NavBar.css";
 
 const NavBar = () => {
     return(
-        <div>
-            <div id="bar-title">
-                <Microscope id="microscope"></Microscope>
-                <h1>CellTracker Pro</h1>
-            </div> 
+        <div id="navbar">
+            <div id="navbar-title">
+                <Microscope id="navbar-icon" />
+                <h1 id="navbar-name">CellTracker Pro</h1>
+            </div>
+
+            <div className="divider"></div>
+
+            <ul id="navbar-menu">
+                <li>
+                    <a href="#">File</a>
+                    {/* <ul id="dropdown-menu">
+                        <li><a href="">New</a></li>
+                        <li><a href="">Open</a></li>
+                        <li><a href="">Open Next</a></li>
+                        <li><a href="">Open Recent</a></li>
+                        <li><a href="">Open Folder</a></li>
+                        <li><a href="">Close</a></li>
+                        <li><a href="">Close All</a></li>
+                        <li><a href="">Save</a></li>
+                        <li><a href="">Revert</a></li>
+                        <li><a href="">Page Setup</a></li>
+                        <li><a href="">Quit</a></li>
+                    </ul> */}
+                </li>
+
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Image</a></li>
+                <li><a href="#">Analyze</a></li>
+                <li><a href="#">Process</a></li>
+            </ul>
+
+            <div className="divider"></div>
+                
+            <div className="navbar-menu-item">
+                <BarChart3 className="menu-icon" ></BarChart3>
+                <a href="#">Result</a>
+            </div>
+
+            <div className="navbar-menu-item">
+                <Search className="menu-icon"></Search>
+                <a href="#">Article</a>
+            </div>
+            
         </div>
+
     );
 }
 export default NavBar;
